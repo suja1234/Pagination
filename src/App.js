5a404192b39353b8c7881d26dfd75e1744777530
suja1234 <Sujatampatil13@gmail.com>
2021-05-30 17:52:03 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./components/style.css"
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(10);
+  const showMoreItems = () => {
+    setVisible((prevValue) => prevValue + 10);
+  };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {
+          items.slice(0,visible).map((item) => (
+            <div className="card">
+              <div className="id">
+                <span>{item.id}</span>
+                <p>{item.body}</p>
+
+               </div>
+            </div>
+            
+          )
+          )
+        }
+        <button onClick={showMoreItems}>Paginated API</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; 
+
+
